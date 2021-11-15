58f7aee5dd1a2eb4dbde4ee0093b3ac94492dd4a
Jan Jakeš <medioncer@gmail.com>
2021-11-15 15:11:14 +0100

    simplicity update

--- CypherAndDecypher/Models/CypherDecypher.cs
+++ CypherAndDecypher/Models/CypherDecypher.cs
@@ -15,12 +15,10 @@ namespace CypherAndDecypher.Models
         public string cypherSubmit { get; set; }
         public string cypherDropFrom { get; set; }
         public string cypherDropTo { get; set; }
-        private static Morse morse = new Morse();
-        private static Caesar caesar = new Caesar();
         public Dictionary<string, ICyphers> cyphers = new Dictionary<string, ICyphers>
         {
-            {"Morse", morse },
-            {"Caesar", caesar }
+            {"Morse", new Morse() },
+            {"Caesar", new Caesar() }
         };
         
     }
